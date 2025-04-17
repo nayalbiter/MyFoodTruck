214441c1a37604844be4d8ca875609dfb527f7ac
nayalbiter <nay.abc25@gmail.com>
2025-04-16 19:37:08 -0700

    added user location and 2 food trucks markers hard coded

--- creatingGoogleMap.js
+++ creatingGoogleMap.js
@@ -1,30 +1,61 @@
-let newMap;
+let userLocationIcon = document.createElement("img");
+userLocationIcon.src = "user.png";
+userLocationIcon.style.width = "40px";
+userLocationIcon.style.height = "40px";
 
-let foodTruckIcon = document.createElement("img"); //icon to the used for the marker
-foodTruckIcon.src = "food-truck.png";
-foodTruckIcon.style.width = "40px"; 
-foodTruckIcon.style.height = "40px";
+let foodTruckIcon1 = document.createElement("img");
+foodTruckIcon1.src = "food-truck.png";
+foodTruckIcon1.style.width = "40px";
+foodTruckIcon1.style.height = "40px";
+
+let foodTruckIcon2 = document.createElement("img");
+foodTruckIcon2.src = "food-truck.png";
+foodTruckIcon2.style.width = "40px";
+foodTruckIcon2.style.height = "40px";
+
+let newMap;
 
 //this function is used to create a map
 async function initMap() {
-    let foodTruckLocation = { lat: 47.608893691960496, lng: -122.14349921135684 };
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    let userLocation = { lat: 47.67731658055798, lng: -122.12261041757242 }
 
-    //Create map object from the maps javascript API:
+    //google libraries
+    const mapLibrary = await google.maps.importLibrary("maps");
+    const Map = mapLibrary.Map;
+    const markerLibrary = await google.maps.importLibrary("marker");
+    const AdvancedMarkerElement = markerLibrary.AdvancedMarkerElement;
+
+    //Create map object:
     newMap = new Map(document.getElementById("map"), {
-        zoom: 17,
-        center: foodTruckLocation,
-        mapId: "la_chingona_taqueria",
+        zoom: 15,
+        center: userLocation,
+        mapId: "user location",
     });
 
-    //Create the marker object for the food truck:
-    const marker = new AdvancedMarkerElement({
+    //marker object for the user location:
+    let userMarker = new AdvancedMarkerElement({
         map: newMap,
-        position: foodTruckLocation,
-        title: "La chingona taqueria",
-        content: foodTruckIcon
-        
+        position: userLocation,
+        title: "My location",
+        content: userLocationIcon
+    });
+
+    //marker object for the food trucks:
+    foodTruck1 = { lat: 47.67601522248604, lng: -122.12250027702324 }
+    let foodTruckMarker1 = new AdvancedMarkerElement({
+        map: newMap,
+        position: foodTruck1,
+        title: "Bandido Mexican Grill",
+        content: foodTruckIcon1
+    });
+
+    //marker object for the food trucks:
+    foodTruck2 = { lat: 47.680114505286454, lng: -122.12521813498681 }
+    let foodTruckMarker2 = new AdvancedMarkerElement({
+        map: newMap,
+        position: foodTruck2,
+        title: "Los Chilangos",
+        content: foodTruckIcon2
     });
 }
 
