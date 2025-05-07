8d8552f402eb60f1a577d1bc80036381c63b464c
nayalbiter <nay.abc25@gmail.com>
2025-05-07 01:15:35 -0700

    the geocoding is working now, hard coded :)

--- static/js/createGoogleMapThatShowsFoodTrucks.js
+++ static/js/createGoogleMapThatShowsFoodTrucks.js
@@ -12,6 +12,7 @@ foodTruckIcon.style.width = "40px";
 foodTruckIcon.style.height = "40px";
 
 let newMap;
+let geocoder;
 
 foodTrucksList = [
     {
@@ -22,18 +23,22 @@ foodTrucksList = [
         title: "Los Chilangos",
         position: { lat: 47.680114505286454, lng: -122.12521813498681 },
     }
-]
+] //to do: connect to database to bring food trucks addresses and names
 
-//this function is used to create a map
+//create a map
 async function initMap() {
 
-    let userLocation = { lat: 47.67731658055798, lng: -122.12261041757242 }
+    let userLocation = { lat: 47.67731658055798, lng: -122.12261041757242 } //to do: use the geolocation api here
 
     //google libraries
     const mapLibrary = await google.maps.importLibrary("maps");
     const Map = mapLibrary.Map;
     const markerLibrary = await google.maps.importLibrary("marker");
     const AdvancedMarkerElement = markerLibrary.AdvancedMarkerElement;
+
+    //code for geocoding------! 
+    const geocodingLibrary = await google.maps.importLibrary("geocoding");
+    geocoder = new google.maps.Geocoder();
 
     //Create map object:
     newMap = new Map(document.getElementById("map"), {
@@ -63,5 +68,44 @@ async function initMap() {
 
 }
 
-initMap();
+//code for geocoding---------------!
+let foodTruckName = "La Chingona Taqueria Seattle";
+let foodTruckAddress = "2940 SW Avalon Way, Seattle, WA 98126";
+
+//geocoding function hard coded right now
+async function geoCodingAddress(foodTruckName, foodTruckAddress) {
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    geocoder.geocode({
+        address: foodTruckAddress,
+        region: "US",
+        componentRestrictions: {
+            country: "US",
+            administrativeArea: "WA"
+        }
+    }, function (results, status) {
+
+        if (status === 'OK') {
+            let sameFoodTruckIcon = foodTruckIcon.cloneNode(true);
+            new AdvancedMarkerElement({
+
+                map: newMap,
+                title: foodTruckName,
+                position: results[0].geometry.location,
+                content: sameFoodTruckIcon
+            });
+            console.log(results[0]);
+            console.log(results);
+
+        } else {
+            alert('something is wrong with the geocode function: ' + status);
+        }
+    });
+}
+
+
+initMap().then(() => {
+    geoCodingAddress(foodTruckAddress, foodTruckName);
+});
+
 
